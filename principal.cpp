ba8b6c88397cc2664e8427f70fe32bd02fa9ada4
Bryanm09 <bmuniandlinux@gmail.com>
2024-03-01 11:56:34 -0500

    Menu

--- principal.cpp
+++ principal.cpp
@@ -1,3 +1,12 @@
 #include<iostream>
 #include<cmath>
 #include<cstdlib>
+
+int menu{
+    int opc;
+    std::cout<<"::::Menu calculadora::::"<<std::endl;
+    std::cout<<"\t1. Suma \n\t2.Resta \n\t3. Multiplicación \n\t4.División"<<std::endl;
+    std::cin>>opc;
+
+    return 0;
+}
